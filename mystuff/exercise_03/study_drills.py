208cb6922024c828c949ba3fb9877c3b8c448865
Jagger Kyne <jagger.kyne@gmail.com>
2014-04-27 12:31:39 +0800

    Finish exercise 3.

---
+++ mystuff/exercise_03/study_drills.py
@@ -0,0 +1,18 @@
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2013 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+print "This is what I need to calculate:"
+
+print 5 * 20 + 294 -2 + 94 % 3
+
+print "Am I correct?"
+
+print "Let's have some floating number:"
+
+print 83 - 0.323 % 3 + 3.13
+
+print "Can Python have fraction? Let's find out!"
+
+print 7 - 1/2
