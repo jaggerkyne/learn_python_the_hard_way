9f21e974a57999012fbb788001aee16ebd6446b4
Jagger Kyne <jagger.kyne@gmail.com>
2014-04-27 12:29:50 +0800

    Finish exercise 3.

---
+++ mystuff/exercise_03/ex3.py
@@ -0,0 +1,44 @@
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2013 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# Math
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30/6 # 30
+
+print "Roosters", 100 - 25 *3 % 4 # 97
+
+print "Now I will count the eggs:"
+
+print 3 + 2 +1 -5 + 4 % 2 - 1/4 + 6 # 7-1/4 = 7
+
+print (7-0.25) # prints out 6.75
+
+print (7-1/4) # prints out 7
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7 # false
+
+print "What is 3 + 2?", 3 + 2 # 5
+
+print "what is 5 -7?", 5 -7 # -2
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 # true
+
+print "Is it greater or equal?", 5 >= -2 # true
+
+print "Is it less or equal?", 5 <= 2 # false
+
+print "What is 7 / 4?", 7/4 # 1
+
+print "What is 7.0 / 4.0?", 7.0/4.0 #1.75
+
+print "Is 7/4 different than 7.0/4.0?", 7/4!=7.0/4.0 # true
