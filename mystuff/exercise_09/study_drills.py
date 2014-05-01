f0070a690bdec88e555120f366d07e4a47bb25ec
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-01 17:10:24 +0800

    Finish exercise 9.

---
+++ mystuff/exercise_09/study_drills.py
@@ -0,0 +1,15 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# 1 Do your checks of your work, write down your mistakes, and try not to make them on the next exercise.
+
+# 2 What if I wanted to start the months on a new line?
+# "\n" is a new line character
+# when using \n with %r, \n will not work
+
+# pay attention to your spelling, it would save you a lot of time and headache later.
+
+
