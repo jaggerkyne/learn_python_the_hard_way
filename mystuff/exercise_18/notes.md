64ac489a364d3b48487085907b06182f82d0bb2b
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-03 15:43:17 +0800

    Finish exercise 18.

---
+++ mystuff/exercise_18/notes.md
@@ -0,0 +1,7 @@
+Notes for Exercise 18
+=====================
+
+Function do three things:
+    1. They name pieces of code the way variables name strings and numbers.
+    2. They take arguments the way your script take argv.
+    3. Using #1 and #2, they let you make your own "mini-scripts" or "tiny commands".
---
+++ mystuff/exercise_18/study_drills.py
@@ -0,0 +1,21 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+# write function
+# 1. def
+# 2. yes
+# 3. yes
+# 4. inside of () and separate with ','
+# 5. yes
+# 6. yes
+# 7. yes
+# 8. yes
+
+# use function
+
+# 1. yes
+# 2. yes
+# 3. yes
+# 4. yes
