23d6bf01ac8d0def0ec7eed7234f6782f4eb7929
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-11 22:40:37 +0800

    Finish exercise 29.

---
+++ mystuff/exercise_29/study_drills.py
@@ -0,0 +1,12 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+# 1. What do you think the if does to the code under it?
+# 2. Why does the code under the if need to be indented four spaces?
+# 3. What happens if it isn't indented?
+# 4. Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+# 5. What happens if you change the initial variables for people, cats and dogs?
+
+
