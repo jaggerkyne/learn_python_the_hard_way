e5fe00286617b36ca64ded9292f1fc3b6a2fddb0
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-11 12:17:22 +0800

    Finish exercise 24.

--- mystuff/__init__.py
+++
@@ -1,6 +0,0 @@
-#--coding: utf-8 --
-# Another code written by Jagger Kyne
-# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
-__author__ = 'Jagger Kyne'
-
-
---
+++ mystuff/exercise_24/study_drills.py
@@ -0,0 +1,9 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# 1. Make sure to do your checks: read it backward, read it out loud, and put comments above confusing parts.
+
+# 2. Break the file on purpose, then run it to see what kinds of errors you get. Make sure you can fix it.
