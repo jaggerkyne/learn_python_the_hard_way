ebfc3c2e952beb0e690cf4153f7f6aae69d3655f
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-11 13:24:40 +0800

    Finish exercise 25.

---
+++ mystuff/exercise_25/study_drills.py
@@ -0,0 +1,19 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# 1. Take the remaining lines of the WYSS output and figure out what they are doing.
+# Make sure you understand how you are running your functions in the ex25 module.
+
+# 2. Try doing this: help(ex25) and also help(ex25.break_words). Notice how you get help for your module and how
+# the help os those odd """ strings you put after each function in ex25? Those special strings are called documentation
+# comments and we'll be seeing more of them.
+
+# 3. Typing ex25. is annoying. A shortcut is to do your import like this: from ex25 import *, which is like saying,
+# "Import everything from ex25." Programmers like saying things backwards. Start a new session and see how all your
+# functions are right there.
+
+# 4. Trying breaking your file and see what is looks like in Python when you use it. You will have to quit Python
+# with CTRL-D(CTRL-Z on Windows) to be able to reload it.
