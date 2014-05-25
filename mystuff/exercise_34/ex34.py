90aff44a33b6586ce65c0cd33e81556cfba9538f
Jagger Kyne <jagger.kyne@gmail.com>
2014-05-25 16:42:21 +0800

    Finish exercise 34.

---
+++ mystuff/exercise_34/ex34.py
@@ -0,0 +1,34 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# People think in Ordinal manner, but programmers think in cardinal.
+# ordinal == ordered, 1st; cardinal == cards at random, starts from 0.
+
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+# The animal at 1 is python
+print "the animal at 1 is ", animals[1]
+
+# the third animal is peacock
+print "The third animal is ", animals[2]
+
+# the first animal is bear
+print "The first animal is ", animals[0]
+
+# the animal at 3 is Kangaroo
+print "The animal at 3 is ", animals[3]
+
+# the fifth animal is whale
+print "The fifth animal is ", animals[4]
+
+# the animal at 2 is peacock
+print "The animal at 2 is ", animals[2]
+
+# the sixth animal is platypus
+print "The sixth animal is ", animals[5]
+
+# the animal at 4 is whale
+print "The animal at 4 is ", animals[4]
---
+++ mystuff/exercise_34/study_drills.py
@@ -0,0 +1,25 @@
+#--coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+# 1. Read about ordinal and cardinal numbers online
+# http://www.mathsisfun.com/numbers/cardinal-ordinal-nominal.html
+# Cardinal: how many; is for counting, no fractions and decimal
+# Ordinal: position; is for the physical position orders of objects
+
+
+# 2. With what you know of the difference between these types of numbers, can you explain why the year 2010 in
+# "January 1, 2010," really is 2010 and not 2009? (Hint: you can't pick years at random)
+# "January 1, 2010" is cardinal as it is for counting the days after the birth of Jesus Christ. Year 2010 cannot be fractionlized.
+
+
+# 3. Write some more lists and work out similar indexes until you can translate them.
+
+fruits = ['apple','strawberry','pear','peach','banana','kiwi fruit']
+
+# 4. Use python to check your answer to this as well.
+
+print "The first position of the fruits is %s " % fruits[0]
+print "The index of pear is %d " % fruits.index('pear')
