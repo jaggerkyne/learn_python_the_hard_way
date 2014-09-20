7fe1802d07d20084aa9518fc4e8acecfb6eb0862
Jagger Kyne <jagger.kyne@gmail.com>
2014-09-20 17:53:53 +0800

    working on exercise 47.

---
+++ mystuff/exercise_47/ex47/game.py
@@ -0,0 +1,17 @@
+# --coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+class Room(object):
+    def __int__(self,name,description):
+        self.name = name
+        self.description = description
+        self.paths = []
+
+    def go(self,direction):
+        return self.paths.get(direction,None)
+
+    def add_paths(self,paths):
+        self.paths.update(paths)
