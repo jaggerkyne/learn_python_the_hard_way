718baf29681e803db63316fe37f09105772f0a60
Jagger Kyne <jagger.kyne@gmail.com>
2014-09-20 16:19:12 +0800

    Finish exercise 46, the quiz is not yet done.

---
+++ mystuff/exercise_46/projects/skeleton/tests/NAME_tests.py
@@ -0,0 +1,18 @@
+# --coding: utf-8 --
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2014 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+
+from nose.tools import *
+import NAME
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
+
